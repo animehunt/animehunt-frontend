2c7c5b2dce780141f8afcf13637c12f6ada81ca0
animehunt <animehunt715@gmail.com>
2026-04-26 03:06:44 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+import { getPage } from "./core/router.js";
+import { initHome } from "./features/homepage.js";
+import { initAnimePage } from "./features/animePage.js";
+import { initCategoryPage } from "./features/categoryPage.js";
+
+const page = getPage();
+
+document.addEventListener("DOMContentLoaded", () => {
+  // ===== HOME =====
+  if (page === "home") {
+    initHome();
+  }
+
+  // ===== TYPE PAGES =====
+  if (page === "anime") initAnimePage("anime");
+  if (page === "movies") initAnimePage("movie");
+  if (page === "series") initAnimePage("series");
+  if (page === "cartoon") initAnimePage("cartoon");
+
+  // ===== CATEGORY PAGE =====
+  const grid = document.querySelector(".anime-grid");
+
+  if (grid?.dataset.category) {
+    initCategoryPage(grid.dataset.category);
+  }
+});
