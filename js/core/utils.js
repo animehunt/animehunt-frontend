a37b9c5952b900acc61b241304058df48c92731a
animehunt <animehunt715@gmail.com>
2026-04-26 03:12:11 +0530

    Update utils.js

--- js/core/utils.js
+++ js/core/utils.js
@@ -1,7 +1,24 @@
 export function createCard(a) {
   return `
     <div class="movie-card" onclick="location.href='details.html?id=${a.id}'">
-      <img src="${a.poster}" />
+      <img data-src="${a.poster}" class="lazy-img" />
     </div>
   `;
+}
+
+// LAZY LOAD
+export function initLazy() {
+  const imgs = document.querySelectorAll(".lazy-img");
+
+  const obs = new IntersectionObserver(entries => {
+    entries.forEach(e => {
+      if (e.isIntersecting) {
+        const img = e.target;
+        img.src = img.dataset.src;
+        obs.unobserve(img);
+      }
+    });
+  });
+
+  imgs.forEach(img => obs.observe(img));
 }
