4605091d0b62a8600a4b51aeedfbe06b7e94db94
animehunt <animehunt715@gmail.com>
2026-04-26 03:11:10 +0530

    Update animePage.js

--- js/features/animePage.js
+++ js/features/animePage.js
@@ -1,14 +1,28 @@
 import { getAnime } from "../core/api.js";
 import { createCard } from "../core/utils.js";
+import { renderPagination } from "./pagination.js";
 import { loadBanner } from "./banner.js";
+
+let currentPage = 1;
 
 export async function initAnimePage(type) {
   await loadBanner(type);
 
   const grid = document.querySelector(".anime-grid");
-  if (!grid) return;
+  const pagination = document.querySelector(".pagination");
 
-  const data = await getAnime(`?type=${type}&limit=40`);
+  async function load(page = 1) {
+    currentPage = page;
 
-  grid.innerHTML = (data || []).map(createCard).join("");
+    const res = await getAnime(`?type=${type}&page=${page}&limit=20`);
+
+    const data = res?.data || res;
+    const total = res?.totalPages || 5;
+
+    grid.innerHTML = (data || []).map(createCard).join("");
+
+    renderPagination(pagination, currentPage, total, load);
+  }
+
+  load(1);
 }
