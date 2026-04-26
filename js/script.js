a0567e3a72a614b21463c6f842d44ee1b2a59f19
animehunt <animehunt715@gmail.com>
2026-04-26 21:20:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ import { initHome } from "./features/homepage.js";
 import { initAnimePage } from "./features/animePage.js";
 import { initCategoryPage } from "./features/categoryPage.js";
 import { initSearch } from "./features/search.js";
+import { initDetailsPage } from "./features/detailsPage.js";
+import { initWatchPage } from "./features/watchPage.js";
 
 const page = getPage();
 
 document.addEventListener("DOMContentLoaded", () => {
-  initSearch(); // 🔍 always
+  initSearch();
 
   if (page === "home") initHome();
 
@@ -15,6 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
   if (page === "movies") initAnimePage("movie");
   if (page === "series") initAnimePage("series");
   if (page === "cartoon") initAnimePage("cartoon");
+
+  if (page === "details") initDetailsPage();
+  if (page === "watch") initWatchPage();
 
   const grid = document.querySelector(".anime-grid");
   if (grid?.dataset.category) {
