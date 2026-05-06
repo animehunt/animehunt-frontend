5b753cef4881bfb37b797183b1ee1073619cbfa8
animehunt <animehunt715@gmail.com>
2026-05-06 22:37:14 +0530

    Update utils.js

--- js/core/utils.js
+++ js/core/utils.js
@@ -1,24 +1,41 @@
-export function createCard(a) {
+export function createCard(anime){
+
   return `
-    <div class="movie-card" onclick="location.href='details.html?id=${a.id}'">
-      <img data-src="${a.poster}" class="lazy-img" />
-    </div>
+    <a href="details.html?slug=${anime.slug}" class="rel-card">
+      <img 
+        src="${anime.poster}?tr=w-300,h-450"
+        style="width:100%;height:100%;object-fit:cover;border-radius:8px"
+      >
+    </a>
   `;
 }
 
-// LAZY LOAD
-export function initLazy() {
-  const imgs = document.querySelectorAll(".lazy-img");
+export function debounce(fn,delay=400){
 
-  const obs = new IntersectionObserver(entries => {
-    entries.forEach(e => {
-      if (e.isIntersecting) {
-        const img = e.target;
-        img.src = img.dataset.src;
-        obs.unobserve(img);
-      }
-    });
+  let timer;
+
+  return (...args)=>{
+    clearTimeout(timer);
+
+    timer = setTimeout(()=>{
+      fn(...args);
+    },delay);
+  };
+}
+
+export function groupBySeason(list=[]){
+
+  const map = {};
+
+  list.forEach(ep=>{
+
+    const s = ep.season || "1";
+
+    if(!map[s]) map[s] = [];
+
+    map[s].push(ep);
+
   });
 
-  imgs.forEach(img => obs.observe(img));
+  return map;
 }
